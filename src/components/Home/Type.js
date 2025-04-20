d00cc6d33aeb40d32ffca79427e5bec05d1ec25b
yelyak <kayley@hklyfe.com>
2025-04-20 07:19:15 +0000

    replit/name/navbar

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,7 +6,7 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
+          "Student",
           "Freelancer",
           "MERN Stack Developer",
           "Open Source Contributor",
