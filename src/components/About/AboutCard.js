205020b22895d6b3843f613f9b15150e99446ab6
yelyak <kayley@hklyfe.com>
2025-05-11 06:40:06 +0000

    everything updated + mort

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Kayley Kwok </span>
+            from <span className="purple"> Hong Kong, China.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a highschool student studying in VSA, and in the 2028 SPARC program.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I speak Chinese, Cantonese, English fluently; and learning Spanish. 
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Play Piano
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Creative Writing
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Read Outdated Non-Fic Books
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Life has no limitations, except the ones you make."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Les Brown</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/mort.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -22,33 +22,32 @@ function Home2() {
               I fell in love with programming and I have at least learnt
               something, I think… 🤷‍♂️
               <br />
-              <br />I am fluent in classics like
+              <br />I practice in programming languages like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Javascript, React, and Python. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of interest's are coding &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Games, Convenience apps </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Game Theory, Student Life, and AI.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              In the future, I'd like to apply my knowledge to projects relating to bridging the gap between<b className="purple">students</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  AI
                 </b>
               </i>
-              &nbsp; like
+              &nbsp; pioneering useful skills to learn such as
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> AI prompting and coding.</b>
               </i>
             </p>
           </Col>
